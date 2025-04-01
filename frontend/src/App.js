48d2aa540fead7eb7957e6f5b1932657964567aa
Mark Oh <mark.oh@mail.utoronto.ca>
2025-04-01 11:03:17 -0400

    first draft

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import page components
 import LogIn from './pages/LogIn.jsx';
+import Promotions from './pages/Promotions.jsx';
 
 const App = () => {
-  
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LogIn />} />
+        <Route path="/promotions" element={<Promotions />} />
+
         {/* Add more routes as needed */}
       </Routes>
     </Router>
