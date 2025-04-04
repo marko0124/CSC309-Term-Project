5a09cdd75c0111d6fb8eff9e7e95e0141719a3a8
matkuang <100985622+matkuang@users.noreply.github.com>
2025-04-03 23:47:19 -0400

    added transactions route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import page components
 import LogIn from './pages/LogIn.jsx';
+import Home from './pages/Home.jsx';
+import Transactions from './pages/transactions';
 
 const App = () => {
   
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LogIn />} />
-        {/* Add more routes as needed */}
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LogIn />} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/transactions" element={<Transactions/>} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
   );
 };
 
