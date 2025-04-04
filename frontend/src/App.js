b028187a2e554c4a7888d3a2b0086c482479dd86
Justin Chang <justin.chang0112@gmail.com>
2025-04-04 15:50:27 -0400

    completed login functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Import page components
 import LogIn from './pages/LogIn.jsx';
+import AuthProvider from './context/authContext.js';
 
 const App = () => {
+
+  const AppRoutes = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<LogIn />} />
+      </Routes>
+    )
+  }
   
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<LogIn />} />
-        {/* Add more routes as needed */}
-      </Routes>
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </Router>
   );
 };
