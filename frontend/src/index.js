ddeca39d266a79591d52535d8fdbc7c269d39cd9
Mark Oh <mark.oh@mail.utoronto.ca>
2025-04-08 18:30:36 -0400

    new branchg

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useAuth } from '../../context/authContext';
+import ManagerPromotions from "./components/Manager/ManagerPromotions";
+import RegularPromotions from "./components/Regular/RegularPromotions";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Promotions = () => {
+  const { user } = useAuth();
+  console.log("Current user:", user); // Debugging line
+  console.log("User role:", user ? user.role : "No user"); // Debugging line
+  console.log("Current user:", user); // Add this for debugging
+  
+  // Check if user is a manager or admin
+  const isManager = user && (user.role === 'manager' || user.role === 'admin');
+  
+  console.log("Is manager:", isManager); // Add this for debugging
+  
+  return isManager ? <ManagerPromotions /> : <RegularPromotions />;
+}
+
+export default Promotions;
