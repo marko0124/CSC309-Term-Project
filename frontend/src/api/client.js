7ca17b296c86d77cca03c88ee460a98f6fa40fa8
matkuang <100985622+matkuang@users.noreply.github.com>
2025-04-09 20:29:28 -0400

    fixed backend url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import env from "react-dotenv";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_BACKEND_URL,
+  baseURL: env.BACKEND_URL,
   headers: {
     'Content-Type': 'application/json'
   },
